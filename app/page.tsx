ba7d2e4cb66142c86bb8e5697678b71f3129e9cd
jnaiburg <32399054+jnaiburg@users.noreply.github.com>
2026-01-19 15:37:52 -0500

    eslint and prettier (#3)

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
-'use client';
+"use client";
 
 import { useRouter } from "next/navigation";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 
 export default function Home() {
-  const router = useRouter();
-  const handleNavigateToGame = () => {
-    router.push("/game-home");
-  };
-  return (
-    <>
-      <h1>Playoff Fantasy Game</h1>
-      <Button variant="primary" onClick={handleNavigateToGame}>Click to navigate to game home</Button>
-    </>
-  );
+	const router = useRouter();
+
+	const handleNavigateToGame = () => {
+		router.push("/game-home");
+	};
+	return (
+		<>
+			<h1>Playoff Fantasy Game</h1>
+			<Button variant="primary" onClick={handleNavigateToGame}>
+				Click to navigate to game home
+			</Button>
+		</>
+	);
 }
