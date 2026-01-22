eb17c01be7ce49e09fbbad906d1673645848a31f
jnaiburg <32399054+jnaiburg@users.noreply.github.com>
2026-01-21 20:39:30 -0500

    NYG-12: add jest tests and ci workflow (#6)

--- app/services/userService.test.ts
+++ app/services/userService.test.ts
@@ -1,1 +1,17 @@
-//TODO: Add tests for userService
+import { getUserById } from "./userService";
+
+let mockUserId: string;
+describe("userService", () => {
+	beforeEach(() => {
+		mockUserId = "1";
+	});
+
+	it("should return a user by id", () => {
+		const user = getUserById(mockUserId);
+		expect(user.id).toBe(mockUserId);
+	});
+
+	it("should throw an error if the user does not exist", () => {
+		expect(() => getUserById("3")).toThrow("User with id 3 not found");
+	});
+});
